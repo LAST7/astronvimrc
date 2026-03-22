bf05654a678b28a16e530bb44cac68f0b3e7fc6b
Last <last7617@outlook.com>
2026-03-23 01:57:06 +0800

    new todo-comment item
    shit & question

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/todo-comments.nvim",
+    opts = {
+        keywords = {
+            SHIT = { icon = " ", color = "shit", alt = { "UGLY", "TRASH", "GARBAGE" } },
+            QUESTION = { icon = " ", color = "question", alt = { "Q", "WTF", "WHAT" } },
+        },
+        colors = {
+            shit = "#db3b49",
+            question = "#74e060"
+        },
+    },
+}
